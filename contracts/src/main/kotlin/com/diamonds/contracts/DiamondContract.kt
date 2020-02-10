de29ff24c9d006cebfd284e43cb099d42757c73b
Priyav Shah <priyavshah@19DUB-MAC03.local>
2020-02-10 14:45:08 +0000

    Completed mine contract verification and command

--- contracts/src/main/kotlin/com/diamonds/contracts/DiamondContract.kt
+++ contracts/src/main/kotlin/com/diamonds/contracts/DiamondContract.kt
@@ -1,10 +1,15 @@
 package com.diamonds.contracts
 
+import com.diamonds.states.DiamondState
+import com.diamonds.states.DiamondType
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.requireSingleCommand
 import net.corda.core.contracts.requireThat
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.transactions.LedgerTransaction
+import java.security.PublicKey
 
 // ************
 // * Contract *
@@ -24,8 +29,14 @@ class DiamondContract : Contract {
         // Verification logic goes here.
         when (command.value) {
             is Commands.Mine -> {
+                val outputDiamond = tx.outputsOfType<DiamondState>()
                 requireThat {
                     "No inputs should be present" using (tx.inputs.size == 0)
+                    "Expected at least one or more diamond outputs" using (outputDiamond.size > 0)
+                    // TODO: Should we make sure no dodgy diamond states are in? "Expected only diamond objects in outputs"
+                    "Diamond Owners must have signed transaction" using (command.signers.toSet().containsAll(
+                            outputDiamond.fold(listOf<PublicKey>()) { participants, diamond -> participants + diamond.participants.map { it.owningKey } }))
+                    "Diamond output state must be of type ROUGH" using (outputDiamond.all { it.state == DiamondType.ROUGH })
                 }
             }
             is Commands.Sell -> {
--- contracts/src/test/kotlin/com/diamonds/contracts/ContractTests.kt
+++ contracts/src/test/kotlin/com/diamonds/contracts/ContractTests.kt
@@ -1,26 +1,29 @@
 package com.diamonds.contracts
 
 import com.diamonds.states.DiamondState
+import com.diamonds.states.DiamondType
+import net.corda.core.contracts.ContractState
 import net.corda.core.contracts.TypeOnlyCommandData
+import net.corda.core.identity.AbstractParty
 import net.corda.testing.node.MockServices
 import net.corda.testing.node.ledger
 import org.junit.Test
 
 class ContractTests {
     private val ledgerServices = MockServices(listOf("com.diamonds.contracts"))
+    val defaultDiamond = DiamondState(owner = ALICE.party)
 
-    val diamond = DiamondState(owner = ALICE.party)
     @Test
     fun shouldOnlyWorkWithTheCorrectCommandOptions() {
         class DummyCommand : TypeOnlyCommandData()
         ledgerServices.ledger {
             transaction {
-                output(DiamondContract.ID, diamond)
+                output(DiamondContract.ID, defaultDiamond)
                 command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
                 this.verifies()
             }
             transaction {
-                output(DiamondContract.ID, diamond)
+                output(DiamondContract.ID, defaultDiamond)
                 command(listOf(ALICE.publicKey), DummyCommand())
                 this.fails()
             }
@@ -31,15 +34,85 @@ class ContractTests {
     fun miningShouldOnlyHave0InputsAndOnly1Output() {
         ledgerServices.ledger {
             transaction {
-                output(DiamondContract.ID, diamond)
+                output(DiamondContract.ID, defaultDiamond)
                 command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
                 this.verifies()
             }
             transaction {
-                output(DiamondContract.ID, diamond)
-                input(DiamondContract.ID, diamond)
+                output(DiamondContract.ID, defaultDiamond)
+                input(DiamondContract.ID, defaultDiamond)
                 command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
                 this.fails()
+            }
+        }
+    }
+
+    @Test
+    fun ownersHaveToSignStones() { // Not sure if its possible to check this in the contract code. :sob:
+        ledgerServices.ledger {
+            transaction {
+                output(DiamondContract.ID, defaultDiamond)
+                output(DiamondContract.ID, DiamondState(owner = BOB.party))
+                command(listOf(ALICE.publicKey, BOB.publicKey), DiamondContract.Commands.Mine())
+                this.verifies()
+            }
+            transaction {
+                output(DiamondContract.ID, defaultDiamond)
+                command(listOf(BOB.publicKey), DiamondContract.Commands.Mine())
+                this.fails()
+            }
+            transaction {
+                output(DiamondContract.ID, defaultDiamond)
+                output(DiamondContract.ID, DiamondState(owner = BOB.party))
+                command(listOf(BOB.publicKey), DiamondContract.Commands.Mine())
+                this.fails()
+            }
+        }
+    }
+
+    @Test
+    fun mustHaveAtleastOneDiamondMined() { // Not sure if its possible to check this in the contract code. :sob:
+        ledgerServices.ledger {
+            transaction {
+                output(DiamondContract.ID, defaultDiamond)
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.verifies()
+            }
+            transaction {
+                output(DiamondContract.ID, defaultDiamond)
+                output(DiamondContract.ID, defaultDiamond)
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.verifies()
+            }
+            transaction {
+                data class dummyContract(override val participants: List<AbstractParty> = listOf()): ContractState
+                output(DiamondContract.ID, dummyContract())
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.fails()
+            }
+        }
+    }
+
+    @Test
+    fun newlyMinedStonesCanOnlyBeOfTypeROUGH() {
+        ledgerServices.ledger {
+            transaction {
+                val diamond = DiamondState(state = DiamondType.POLISHED, owner = ALICE.party)
+                output(DiamondContract.ID, diamond)
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.fails()
+            }
+            transaction {
+                val diamond = DiamondState(state = DiamondType.CUT, owner = ALICE.party)
+                output(DiamondContract.ID, diamond)
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.fails()
+            }
+            transaction {
+                val diamond = DiamondState(state = DiamondType.ROUGH, owner = ALICE.party)
+                output(DiamondContract.ID, diamond)
+                command(listOf(ALICE.publicKey), DiamondContract.Commands.Mine())
+                this.verifies()
             }
         }
     }
