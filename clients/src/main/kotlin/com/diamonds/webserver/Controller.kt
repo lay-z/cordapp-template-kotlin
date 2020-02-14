dbdd26a83a12ef5f5ce787d37e5a14b5917d52c5
priyav shah <priyav.shah@r3.com>
2020-02-14 13:47:10 +0000

    Build 'mine' restful endpoint. and extra utility endpoints

--- clients/src/main/kotlin/com/diamonds/webserver/Controller.kt
+++ clients/src/main/kotlin/com/diamonds/webserver/Controller.kt
@@ -1,5 +1,9 @@
 package com.diamonds.webserver
 
+import com.diamonds.flows.MineDiamondFlow
+import com.diamonds.states.DiamondState
+import net.corda.core.utilities.getOrThrow
+import net.corda.finance.DOLLARS
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
  * Define your API endpoints here.
  */
 @RestController
-@RequestMapping("/") // The paths for HTTP requests are relative to this base path.
+@RequestMapping("/diamonds") // The paths for HTTP requests are relative to this base path.
 class Controller(rpc: NodeRPCConnection) {
 
     companion object {
@@ -18,8 +22,17 @@ class Controller(rpc: NodeRPCConnection) {
 
     private val proxy = rpc.proxy
 
-    @GetMapping(value = ["/templateendpoint"], produces = ["text/plain"])
-    private fun templateendpoint(): String {
-        return "Define an endpoint here."
+    @GetMapping(value = ["/mine"], produces = ["Application/json"])
+    private fun mine(): HashMap<String, String> {
+        val signedTransaction = proxy.startFlowDynamic(MineDiamondFlow::class.java, 100.DOLLARS).returnValue.getOrThrow()
+
+        val diamond = signedTransaction.tx.outputStates.single() as DiamondState
+
+        return hashMapOf(
+                "owner" to diamond.owner.toString(),
+                "value" to diamond.value.toString(),
+                "linearId" to diamond.linearId.toString(),
+                "state" to diamond.state.toString()
+        )
     }
 }
